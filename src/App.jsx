815a9cb8abdea51ce5dd40ea8ed0b637ddcdea69
Amanda <89360779+amandaconstante@users.noreply.github.com>
2026-06-27 22:51:20 -0300

    feat: configura roteamento básico de páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from 'react'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './componentes/Home'
+import CadastroPedido from './componentes/CadastroPedido'
+import CadastroCliente from './componentes/CadastroCliente'
 
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/pedido",
+    element: <CadastroPedido />
+  },
+  {
+    path: "/cliente",
+    element: <CadastroCliente />
+  }
+])
 
 function App() {
 
   return (
     <>
-      <h1>Sistema de Pedidos</h1>
+      {/* <h1>Sistema de Pedidos</h1> */}
+      <RouterProvider router={router}/>
     </>
   )
 }
